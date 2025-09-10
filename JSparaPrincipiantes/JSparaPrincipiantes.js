fae56a9779ef38889175243f4224cb4293601a4a
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-10 16:08:14 -0600

    Estos son los tipos de datos en JavaScript

--- JSparaPrincipiantes/JSparaPrincipiantes.js
+++ JSparaPrincipiantes/JSparaPrincipiantes.js
@@ -1,1 +1,27 @@
-document.write("<h1> Hello World </h1>")
+// Tipos de datos
+"Hello World" // string
+'Hello World' // string
+
+console.log('this is a string');
+
+// Number
+1000000
+-2.3
+
+
+// boolean
+true
+false
+
+// array
+['jungkook','taehyung','jin']
+[1, 2, 3]
+[true, false, true, false]
+
+// object
+{
+    "username": 'jungkook',
+    "score": 80.4,
+    "hours": 14,
+    "proffesional": true,
+}
