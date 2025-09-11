3f57413c8082ebb3ffd2339d5c40547d8cc0b88b
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-10 23:25:46 -0600

    Uso de CONDICIONALES

--- JSparaPrincipiantes/JSparaPrincipiantes.js
+++ JSparaPrincipiantes/JSparaPrincipiantes.js
@@ -1,8 +1,26 @@
-//  OPERADORES
-let passwordDB = 'pepe123';
+// CONDICIONALES
+let typeCard = 'hola';
 
-let input = 'pepe123'
+switch(typeCard) {
+    case 'Debid Card':
+        console.log('This is a debid card');
+        break;
+    case 'Credit Card':
+        console.log('This is a credit card');
+        break;
+    default:
+        console.log('No Card');
+}
 
-let result = input == passwordDB;
 
-console.log(result);
+
+/*let score = 70;
+
+if (score > 30) {
+    console.log('You need to practice more');
+} else if(score > 15) {
+    console.log('Estas mejorando');
+}
+else {
+    console.log('You need to follow this tutorial')
+}*/
