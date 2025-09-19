c79d2fe42855c3536f5164c11b06861552c73162
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-18 21:49:25 -0600

    En esta parte utilice el uso de arrow function para calcular el cuadrado de x números

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -1,4 +1,4 @@
-// Uso de let y const vamos a modificar el código
+// Ejercicio 1 Uso de let y const vamos a modificar el código
 
 let nombre = "Armando"; // usamos let porque se reasigna
 const edad = 25; // usamos const porque no cambia
@@ -7,3 +7,12 @@ nombre = "Ana Maria"; // reasignación permitida porque es let
 
 var saludo = "Hola,  " +  nombre + ". Tienes " +  edad + " años."; 
 console.log(saludo); // muestra el contenido de saludo
+
+
+// Ejercicio 2 Convertir una función tradicional a una arrow function
+ 
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(7)); 
+console.log(cuadrado(2)); 
+console.log(cuadrado(5)); 
