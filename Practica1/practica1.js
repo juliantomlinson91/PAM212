8915540bb58a7becae6bbe36664de75d224ed0d5
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-18 19:05:37 -0600

    Usando las variables modifique el código de el profesor a las necesidades que tenia fue como modifique el código

---
+++ Practica1/practica1.js
@@ -0,0 +1,9 @@
+// Uso de let y const vamos a modificar el código
+
+let nombre = "Armando"; // usamos let porque se reasigna
+const edad = 25; // usamos const porque no cambia
+
+nombre = "Ana Maria"; // reasignación permitida porque es let
+
+var saludo = "Hola,  " +  nombre + ". Tienes " +  edad + " años."; 
+console.log(saludo); // muestra el contenido de saludo
