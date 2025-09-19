6491487c973ee85bf080f05388ca7bd347f2088a
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-18 22:07:14 -0600

    Creación de saludo personalizado utilizando variable const y el uso de arrow function

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -10,9 +10,17 @@ console.log(saludo); // muestra el contenido de saludo
 
 
 // Ejercicio 2 Convertir una función tradicional a una arrow function
- 
+
 const cuadrado = numero => numero * numero;
 
 console.log(cuadrado(7)); 
 console.log(cuadrado(2)); 
 console.log(cuadrado(5)); 
+
+// Ejercicio 3 Crear un arrow function 'Saludo Personalizado'
+
+const saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Julián", 19));
+console.log(saludoPersonalizado("Jungkook", 28));
