a7a92fcef6dbfb892467ee2b15a047567547dc34
juliantomlinson91 <124048762@upq.edu.mx>
2025-09-19 19:18:59 -0600

    En este ejercicio usamos la función .map y filtre los productos con un precio mayor a 1000 y crear un nuevo Array con solo los nombres de los productos

--- Practica2/practica2.js
+++ Practica2/practica2.js
@@ -10,7 +10,19 @@ const persona = {
 };
 // Aplicamos la Destructuración
 const { nombre, edad, direccion:{municipio, ciudad, pais } } = persona;
-console.log("Me llamo  " +  nombre + ", tengo " +  edad + " años y vivo en " + ciudad + ".");
+//console.log("Me llamo  " +  nombre + ", tengo " +  edad + " años y vivo en " + ciudad + ".");
 
 
-// Ejercicio B 
+// Ejercicio B uso de .map()
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+let filtrados = productos.filter(producto => producto.precio > 1000);
+ console.log("Los productos con un precio mayor  a 1000 son: ");
+ console.log(filtrados);
+
+ let nuevoArray = filtrados.map(filtrado => filtrado.nombre);
+ console.log(nuevoArray);
