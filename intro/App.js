e6abf7961a9c2009a0d45ebce02cef8618ad3ee4
juliantomlinson91 <124048762@upq.edu.mx>
2025-10-09 18:09:09 -0600

    Craer boton incrementar y el contador

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,23 @@
+//1. Import: Zona de declaraciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import R,{useState} from 'react';
 
+//2. Main: Zona de componentes
 export default function App() {
+
+  const[contador,setContador]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text> Hola Mundo React Native!!</Text>
+      <Text> Contador: {contador} </Text>
+      <Button title="Incrementar" onPress={()=>setContador(contador+1)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//3. Estilos: Zona de estetica y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
