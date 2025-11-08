71f8420c00fc27696a944bf73eeb58ef114139c0
juliantomlinson91 <124048762@upq.edu.mx>
2025-11-07 18:11:02 -0600

    EXAMEN 2do Parcial 1er commit

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Alert } from 'react-native';
+import { SectionList } from 'react-native-web';
 
 export default function App() {
+
+  const categorias = [
+    {
+      id: 1,
+      titulo: 'K-POP',
+      articulos:['Seventeen', 'StrayKids'],
+      descripcion: '',
+      fechaDePublicacion: '07 de noviembre 2025',
+      resumen: '',
+      imagen: '',
+    },
+    {
+      id: 2,
+      titulo: 'POP',
+      articulos: ['Louis Tomlinson', 'Harry Styles'],
+      descripcion: '',
+      fechaDePublicacion: '07 de diciembre 2025',
+      resumen: '',
+      imagen: '',
+    },
+    {
+      id: 3,
+      titulo: 'Regeeaton',
+      articulos: ['Maluma', 'Rauw Alejandro'],
+      descripcion: '',
+      fechaDePublicacion: '',
+      resumen: '',
+      imagen: '',
+    },
+  ];
+  const SimpleHeader = () => {
+    return (
+      <View style ={styles.header}>
+        <Text style ={styles.header}>Géneros Musicales</Text>
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SimpleHeader/>
+      <ScrollView
+      horizontal ={true}
+      showsHorizontalScrollIndicator={true}
+      contentContainerStyle={styles.scrollContent}
+      >
+        {DATA.map(val => {
+          return (
+            <View style={styles.card} key = {val.id}>
+              <Text style={styles.subtitle}> ¡Soy una tarjeta!</Text>
+            </View>
+          );
+        })}
+
+      </ScrollView>
     </View>
   );
-}
+};
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  }
+  
