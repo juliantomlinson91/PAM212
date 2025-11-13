96b0736fcda3bd0fa34a4004996414bfc26ada06
juliantomlinson91 <124048762@upq.edu.mx>
2025-11-12 22:18:32 -0600

    BottomShet: Completada

--- intro/screens/BottomSheetScreen.js
+++ intro/screens/BottomSheetScreen.js
@@ -1,64 +1,56 @@
-import React, {useRef, useMemo} from 'react'
-import { Text, StyleSheet, View, Button, Image } from 'react-native';
-import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
-import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
+import React, { useRef, useMemo } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function BottomSheetScreen () {
-  const BottomSheetRef = useRef(null);
-  const snapPoints = useMemo(() => [1, '25%', '50%']);
+export default function App() {
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => [1, "25%", "50%"]);
 
-  const openSheet = () => {
-    BottomSheetRef.current?.expand();
+  const handleOpenSheet = () => {
+    bottomSheetRef.current?.expand();
   };
 
-  return(
+  return (
     <GestureHandlerRootView>
-      <View>
-        <Button
-        title='Abrir'
-        onPress={openSheet}
-      />
+      <View style={styles.container}>
+        <Button title="Boton abrir" onPress={handleOpenSheet} />
       </View>
       <BottomSheet
-        ref={BottomSheet}
+        ref={bottomSheetRef}
         snapPoints={snapPoints}
-        enablePanDownToClose = {true}
-        backgroundStyle={style.BSheet}
-      >
-        <BottomSheetView
-          style={style.BView}
+        enablePanDownToClose={true}
+        backgroundStyle={styles.BSheet}
         >
-
-          <Text> BottomSheet Ejemplo!! </Text>
-          <Image 
-            style={style.IMG}
-            source={require('../assets/background.png')}
-          />
-
-        </BottomSheetView>
-
+          <BottomSheetView style = { styles.BView}>
+            <Text>BottomSheet ejemplo</Text>
+            <Image
+            style = {styles.IMG}
+              source = {{uri:'https://imgs.search.brave.com/LPSJcpbXZadTVSDBye2DacITglVdOhdM1KAjB4v1Z5s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA4LzA0LzMwLzgy/LzM2MF9GXzgwNDMw/ODI2Nl9ZeXdZY2xm/RjdhaG05UjZ2dDBV/ZGJpcUFhbEN6WTA1/ei5qcGc'}}
+            />
+          </BottomSheetView>
       </BottomSheet>
     </GestureHandlerRootView>
-  )
+  );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#463c3cff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#3d3535ff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   BSheet: {
-    backgroundColor: '#282727'
+    backgroundColor: '#e7dedeff'
   },
-  BView: {
-    flex: 1,
-    alignItems: 'center'
+  BView:{
+    flex:1,
+    alignItems:'center'
   },
-  IMG: {
-    marginTop: 50,
+  IMG:{
+    marginTop:50,
     width: 200,
-    height:200
+    height: 200
   }
-})
+});
