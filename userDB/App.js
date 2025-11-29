4cd5ab5b9e8869465cb715a01ce90cacfe08e940
juliantomlinson91 <124048762@upq.edu.mx>
2025-11-28 18:24:35 -0600

    CRUD completo funcionando

--- userDB/App.js
+++ userDB/App.js
@@ -1,8 +1,20 @@
-import Screen from './screens/InsertUsuarioScreen'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import InsertUsuarioScreen from './screens/InsertUsuarioScreen';
 
 export default function App() {
   return (
-    <Screen/>
+
+    <InsertUsuarioScreen />
+   
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- userDB/controllers/UsuarioController.js
+++ userDB/controllers/UsuarioController.js
@@ -1,58 +1,82 @@
 import { Usuario } from "../models/usuario";
-import DatabaseService from "../database/DatabaseService";
+import DataBaseServise from "../database/DatabaseService";
 
 export class UsuarioController {
     constructor() {
         this.listeners = [];
     }
 
-    // Inicializar el controlador con el Service
     async initialize() {
-        await DatabaseService.initialize();
+        await DataBaseServise.initialize();
     }
 
     async obtenerUsuarios() {
         try {
-            const data = await DatabaseService.getAll();
+            const data = await DataBaseServise.getAll();
             return data.map(u => new Usuario(u.id, u.nombre, u.fecha_creacion));
         } catch (error) {
-            console.error('Error al obtener usuarios:', error);
-            throw new Error('No se pudieron cargar los usuarios', error);  
+            console.error('Error al obtener los usuarios: ', error);
+            throw new Error('No se pudieron cargar los usuarios');
         }
     }
 
     async crearUsuario(nombre) {
         try {
-            // 1. Validar datos
             Usuario.validar(nombre);
-
-            // 2. Insertar en BD
-            const nuevoUsuario = await DatabaseService.add(nombre.trim());
-
-            // 3. Notificar a los observadores
+            const nuevoUsuario = await DataBaseServise.add(nombre.trim());
             this.notifyListeners();
 
-            // 4. Retornar usuario creado
             return new Usuario(
                 nuevoUsuario.id,
                 nuevoUsuario.nombre,
                 nuevoUsuario.fecha_creacion
             );
         } catch (error) {
-            console.error('Error al crear usuario:', error);
+            console.error('Error al crear usuario: ', error);
             throw error;
         }
     }
 
-    addListener(callBack) {
-        this.listeners.push(callBack);
+    async modificarUsuario(id, nombre) {
+        try {
+            Usuario.validar(nombre);
+            const actualizado = await DataBaseServise.modificar(id, nombre);
+            this.notifyListeners();
+
+            return actualizado
+                ? new Usuario(
+                    actualizado.id,
+                    actualizado.nombre,
+                    actualizado.fecha_creacion
+                )
+                : null;
+
+        } catch (error) {
+            console.error('Error al actualizar usuario: ', error);
+            throw error;
+        }
     }
 
-    removeListener(callBack) {
-        this.listeners = this.listeners.filter(l => l == callback)
-    } 
+    async eliminarUsuario(id) {
+        try {
+            await DataBaseServise.eliminar(id);
+            this.notifyListeners();
+        } catch (error) {
+            console.error('Error al eliminar usuario.', error);
+            throw error;
+        }
+    }
     
+
+    addListener(callback) {
+        this.listeners.push(callback);
+    }
+
+    removeListener(callback) {
+        this.listeners = this.listeners.filter(l => l !== callback);
+    }
+
     notifyListeners() {
-        this.listeners.forEach(callBack => callBack());
+        this.listeners.forEach(callback => callback());
     }
 }
